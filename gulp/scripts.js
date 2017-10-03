9b032c1dba0f47bf776476ff611c4d842bb12459
Ernesto Ortiz <ernesto.tito@gail.com>
2017-10-03 17:31:58 -0500

    New way to handle paths using a new gulp script.
    There are several points to take care.
    
    1.- the command "tsc" will not work for ts compilation, you have to use "gulp test".
    2.- Make sure all new directories does no match with the tsconfig.json path attribute.
    3.- Make sure all ts files does not begin with the path attributes.

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -6,7 +6,6 @@
 var path = require('path'),
 	ts = require('gulp-typescript'),
 	tslint = require('gulp-tslint'),
-	sourcemaps = require('gulp-sourcemaps'),
 	tsimport = require('./convert/ts-import');
 
 module.exports = function (gulp) {
@@ -22,20 +21,21 @@ module.exports = function (gulp) {
 		return gulp.src(cfg.fileset.ts).pipe(tslint({formatter: "prose"})).pipe(tslint.report());
 	});
 
+	/**
+	 * Compile all file with no source maps.
+	 */
 	gulp.task('compile', ['ts-lint'], function (done) {
 
 		var tsProject = ts.createProject('tsconfig.json', {
 			typescript: require('typescript')
 		});
 		gulp.src(path.join(tsProject.config.compilerOptions.rootDir, "**", "*"))
-			.pipe(sourcemaps.init()) // This means sourcemaps will be generated
 			.pipe(tsProject())
 			.pipe(tsimport(tsProject.config.compilerOptions))
 			.on('error', function (error, callback) {
 				console.error(error.stack);
 				this.emit("end");
 			})
-			.pipe(sourcemaps.write()) // sourcemaps are added to the .js file
 			.pipe(gulp.dest(tsProject.config.compilerOptions.outDir))
 			.on("end", done);
 	});
--- gulp/test.js
+++ gulp/test.js
@@ -6,13 +6,35 @@
 var path = require('path'),
     ts = require('gulp-typescript'),
     mocha = require('gulp-mocha'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+	tsimport = require('./convert/ts-import');
 
 module.exports = function (gulp) {
 
     var cfg = gulp.cfg;
 
-    gulp.task('run-tests', ['ts-lint', 'compile'], function () {
+	/**
+	 * Compile with source maps in the js files, useful for debugging.
+	 */
+	gulp.task('compile-test', ['ts-lint'], function (done) {
+
+		var tsProject = ts.createProject('tsconfig.json', {
+			typescript: require('typescript')
+		});
+		gulp.src(path.join(tsProject.config.compilerOptions.rootDir, "**", "*"))
+			.pipe(sourcemaps.init()) // This means sourcemaps will be generated
+			.pipe(tsProject())
+			.pipe(tsimport(tsProject.config.compilerOptions))
+			.on('error', function (error, callback) {
+				console.error(error.stack);
+				this.emit("end");
+			})
+			.pipe(sourcemaps.write()) // sourcemaps are added to the .js file
+			.pipe(gulp.dest(tsProject.config.compilerOptions.outDir))
+			.on("end", done);
+	});
+
+    gulp.task('run-tests', ['ts-lint', 'compile-test'], function () {
         return gulp.src(cfg.fileset.test, {read: false})
             .pipe(mocha({reporter: cfg.mocha.reporter}));
     });
