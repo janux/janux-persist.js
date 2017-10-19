f3742cd77e7284dfc0f5200e5e229ebe0686914c
Ernesto Ortiz <ernesto.tito@gail.com>
2017-10-19 16:34:49 -0500

    Simplify dao factory part 1.

--- src/services/dao-factory/dao-factory-service.ts
+++ src/services/dao-factory/dao-factory-service.ts
@@ -25,10 +25,11 @@ import {AccountDao} from "daos/user/account-dao";
 import {AccountDaoLokiJsImpl} from "daos/user/lokijs/account-dao-lokijs-impl";
 import {AccountDaoMongooseImpl} from "daos/user/mongoose/account-dao-mongoose-impl";
 import {AccountMongooseDbSchema} from "daos/user/mongoose/account-mongoose-schema";
-import * as _ from 'lodash';
+import {DbAdapter} from "persistence/api/db-adapters/db-adapter";
 import {EntityPropertiesImpl} from "persistence/implementations/dao/entity-properties";
 import {LokiJsAdapter} from "persistence/implementations/db-adapters/lokijs-db-adapter";
 import {MongooseAdapter} from "persistence/implementations/db-adapters/mongoose-db-adapter";
+import {DaoSettings} from "services/dao-factory/dao-settings";
 import {DataSource} from "services/datasource-handler/datasource";
 import {DataSourceHandler} from "services/datasource-handler/datasource-handler";
 import * as logger from 'util/logger-api/logger-api';
@@ -39,6 +40,24 @@ import {Dao} from "./dao";
  * Given the database params, this class hides which implementation classes are used.
  */
 export class DaoFactory {
+
+	public static subscribeDao<T>(daoSettings: DaoSettings,
+								  classReference: any): T {
+		this._log.debug("Call to subscribeDao with daoSettings %j", daoSettings);
+		let dbAdapter: DbAdapter;
+		let dao: T;
+		const existingDao: Dao = this.getDao(daoSettings.dbEngine, daoSettings.dbPath, daoSettings.collectionName);
+		if (existingDao == null) {
+			const dataSource: DataSource = this.getDataSource(daoSettings.dbEngine, daoSettings.dbPath);
+			dbAdapter = this.createDbAdapter(dataSource, daoSettings);
+			dao = new classReference(dbAdapter, daoSettings.entityProperties);
+			this.insertDao(daoSettings.dbEngine, daoSettings.dbPath, daoSettings.collectionName, dao);
+		} else {
+			dao = existingDao.daoInstance;
+		}
+		return dao;
+	}
+
 	/**
 	 * Get a new party dao, or a existing one with the same db engine and dbPath.
 	 * @param dbEngine
@@ -47,27 +66,10 @@ export class DaoFactory {
 	 */
 	public static createPartyDao(dbEngine: any, dbPath: string): PartyDao {
 		this._log.debug("Call to createPartyDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		let partyDao: PartyDao;
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.PARTY_DEFAULT_COLLECTION_NAME);
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new partyDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				partyDao = new PartyDaoMongooseImpl(
-					new MongooseAdapter(dataSource.dbConnection.model(this.PARTY_DEFAULT_COLLECTION_NAME, PartyMongooseSchema)),
-					new EntityPropertiesImpl(true, true)
-				);
-			} else {
-				partyDao = new PartyDaoLokiJsImpl(new LokiJsAdapter(this.PARTY_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.PARTY_DEFAULT_COLLECTION_NAME, partyDao);
-			return partyDao;
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.PARTY_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), PartyMongooseSchema), PartyDaoMongooseImpl);
 		} else {
-			this._log.debug("Returning an existing partyDao");
-			partyDao = existingDao.daoInstance;
-			return partyDao;
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.PARTY_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), PartyDaoLokiJsImpl);
 		}
 	}
 
@@ -79,61 +81,10 @@ export class DaoFactory {
 	 */
 	public static createAccountDao(dbEngine: any, dbPath: string): AccountDao {
 		this._log.debug("Call to createAccountDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.ACCOUNT_DEFAULT_COLLECTION_NAME);
-		let accountDao: AccountDao;
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new createAccountDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				accountDao = new AccountDaoMongooseImpl(
-					new MongooseAdapter(dataSource.dbConnection.model(this.ACCOUNT_DEFAULT_COLLECTION_NAME, AccountMongooseDbSchema)),
-					new EntityPropertiesImpl(true, true));
-			} else {
-				accountDao = new AccountDaoLokiJsImpl(
-					new LokiJsAdapter(this.ACCOUNT_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.ACCOUNT_DEFAULT_COLLECTION_NAME, accountDao);
-			this._log.debug("Returning inserted promise");
-			return accountDao;
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.ACCOUNT_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), AccountMongooseDbSchema), AccountDaoMongooseImpl);
 		} else {
-			this._log.debug("Returning an existing accountDao");
-			accountDao = existingDao.daoInstance;
-			return accountDao;
-		}
-	}
-
-	/**
-	 * Gets a group dao.
-	 * @param dbEngine
-	 * @param {string} dbPath
-	 * @return {GroupDao}
-	 */
-	public static createGroupDao(dbEngine: any, dbPath: string): GroupDao {
-		this._log.debug("Call to crateGroupDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.GROUP_DEFAULT_COLLECTION_NAME);
-		let groupDao: GroupDao;
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new groupDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				groupDao = new GroupDao(
-					new MongooseAdapter(dataSource.dbConnection.model(this.GROUP_DEFAULT_COLLECTION_NAME, GroupMongooseSchema)),
-					new EntityPropertiesImpl(true, true));
-			} else {
-				groupDao = new GroupDao(
-					new LokiJsAdapter(this.GROUP_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.GROUP_DEFAULT_COLLECTION_NAME, groupDao);
-			this._log.debug("Returning inserted dao");
-			return groupDao;
-		} else {
-			this._log.debug("Returning an existing groupDao");
-			groupDao = existingDao.daoInstance;
-			return groupDao;
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.ACCOUNT_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), AccountDaoLokiJsImpl);
 		}
 	}
 
@@ -145,28 +96,10 @@ export class DaoFactory {
 	 */
 	public static createAuthContextDao(dbEngine: any, dbPath: string): AuthContextDao {
 		this._log.debug("Call to createAuthContextDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.AUTHCONTEXT_DEFAULT_COLLECTION_NAME);
-		let authContextDao: AuthContextDao;
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new createAuthContextDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				authContextDao = new AuthContextDaoMongooseImpl(
-					new MongooseAdapter(dataSource.dbConnection.model(this.AUTHCONTEXT_DEFAULT_COLLECTION_NAME, AuthContextMongooseDbSchema)),
-					new EntityPropertiesImpl(true, true));
-			} else {
-				authContextDao = new AuthContextDaoLokiJsImpl(
-					new LokiJsAdapter(this.AUTHCONTEXT_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.AUTHCONTEXT_DEFAULT_COLLECTION_NAME, authContextDao);
-			this._log.debug("Returning inserted promise");
-			return authContextDao;
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.AUTHCONTEXT_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), AuthContextMongooseDbSchema), AuthContextDaoMongooseImpl);
 		} else {
-			this._log.debug("Returning an existing AuthContextDao");
-			authContextDao = existingDao.daoInstance;
-			return authContextDao;
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.AUTHCONTEXT_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), AuthContextDaoLokiJsImpl);
 		}
 	}
 
@@ -178,28 +111,25 @@ export class DaoFactory {
 	 */
 	public static createRoleDao(dbEngine: any, dbPath: string): RoleDao {
 		this._log.debug("Call to createRoleDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.ROLE_DEFAULT_COLLECTION_NAME);
-		let RoleDao: RoleDao;
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new createRoleDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				RoleDao = new RoleDaoMongooseImpl(
-					new MongooseAdapter(dataSource.dbConnection.model(this.ROLE_DEFAULT_COLLECTION_NAME, RoleMongooseDbSchema)),
-					new EntityPropertiesImpl(true, true));
-			} else {
-				RoleDao = new RoleDaoLokiJsImpl(
-					new LokiJsAdapter(this.ROLE_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.ROLE_DEFAULT_COLLECTION_NAME, RoleDao);
-			this._log.debug("Returning inserted promise");
-			return RoleDao;
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.ROLE_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), RoleMongooseDbSchema), RoleDaoMongooseImpl);
 		} else {
-			this._log.debug("Returning an existing RoleDao");
-			RoleDao = existingDao.daoInstance;
-			return RoleDao;
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.ROLE_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), RoleDaoLokiJsImpl);
+		}
+	}
+
+	/**
+	 * Gets a group dao.
+	 * @param dbEngine
+	 * @param {string} dbPath
+	 * @return {GroupDao}
+	 */
+	public static createGroupDao(dbEngine: any, dbPath: string): GroupDao {
+		this._log.debug("Call to crateGroupDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.GROUP_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), GroupMongooseSchema), GroupDao);
+		} else {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.GROUP_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), GroupDao);
 		}
 	}
 
@@ -211,59 +141,24 @@ export class DaoFactory {
 	 */
 	public static createGroupContentDao(dbEngine: any, dbPath: string): GroupContentDao {
 		this._log.debug("Call to createGroupContentDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.GROUP_CONTENT_DEFAULT_COLLECTION_NAME);
-		let groupContentDao: GroupContentDao;
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new groupContentDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				groupContentDao = new GroupContentDao(
-					new MongooseAdapter(dataSource.dbConnection.model(this.GROUP_CONTENT_DEFAULT_COLLECTION_NAME, GroupContentMongooseSchema)),
-					new EntityPropertiesImpl(true, true));
-			} else {
-				groupContentDao = new GroupContentDao(
-					new LokiJsAdapter(this.GROUP_CONTENT_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.GROUP_CONTENT_DEFAULT_COLLECTION_NAME, groupContentDao);
-			this._log.debug("Returning new groupContentDao");
-			return groupContentDao;
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.GROUP_CONTENT_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), GroupContentMongooseSchema), GroupContentDao);
 		} else {
-			this._log.debug("Returning an existing groupContentDao");
-			groupContentDao = existingDao.daoInstance;
-			return groupContentDao;
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.GROUP_CONTENT_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), GroupContentDao);
 		}
 	}
 
 	public static createGroupAttributesDao(dbEngine: any, dbPath: string): GroupAttributeValueDao {
 		this._log.debug("Call to createGroupAttributesDao with dbEngine %j , dbPath %j", dbEngine, dbPath);
-		const existingDao: Dao = this.getDao(dbEngine, dbPath, this.GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME);
-		let groupAttributeValueDao: GroupAttributeValueDao;
-		if (_.isUndefined(existingDao)) {
-			this._log.debug("Creating a new groupAttributeValueDao");
-			const dataSource: DataSource = this.getDataSource(dbEngine, dbPath);
-			if (dbEngine === DataSourceHandler.MONGOOSE) {
-				groupAttributeValueDao = new GroupAttributeValueDao(
-					new MongooseAdapter(dataSource.dbConnection.model(this.GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME, GroupAttributeValueMongooseSchema)),
-					new EntityPropertiesImpl(true, true));
-			} else {
-				groupAttributeValueDao = new GroupAttributeValueDao(
-					new LokiJsAdapter(this.GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME, dataSource.dbConnection),
-					new EntityPropertiesImpl(true, true)
-				);
-			}
-			this.insertDao(dbEngine, dbPath, this.GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME, groupAttributeValueDao);
-			this._log.debug("Returning new groupAttributeValueDao");
-			return groupAttributeValueDao;
+		if (dbEngine === DataSourceHandler.MONGOOSE) {
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties(), GroupAttributeValueMongooseSchema), GroupAttributeValueDao);
 		} else {
-			this._log.debug("Returning an existing groupAttributeValueDao");
-			groupAttributeValueDao = existingDao.daoInstance;
-			return groupAttributeValueDao;
+			return this.subscribeDao(new DaoSettings(dbEngine, dbPath, this.GROUP_ATTRIBUTES_DEFAULT_COLLECTION_NAME, EntityPropertiesImpl.createDefaultProperties()), GroupAttributeValueDao);
 		}
 	}
 
 	private static PARTY_DEFAULT_COLLECTION_NAME: string = "contact";
+
 	private static ACCOUNT_DEFAULT_COLLECTION_NAME: string = "account";
 	private static AUTHCONTEXT_DEFAULT_COLLECTION_NAME: string = "authcontext";
 	private static ROLE_DEFAULT_COLLECTION_NAME: string = "role";
@@ -272,6 +167,16 @@ export class DaoFactory {
 	private static GROUP_DEFAULT_COLLECTION_NAME: string = "group";
 	private static _log = logger.getLogger("DaoFactoryService");
 	private static daos: Dao[] = [];
+
+	private static createDbAdapter(dataSource: DataSource, daoSettings: DaoSettings) {
+		let dbAdapter: DbAdapter;
+		if (dataSource.dbEngine === DataSourceHandler.MONGOOSE) {
+			dbAdapter = new MongooseAdapter(dataSource.dbConnection.model(daoSettings.collectionName, daoSettings.schema));
+		} else {
+			dbAdapter = new LokiJsAdapter(daoSettings.collectionName, dataSource.dbConnection);
+		}
+		return dbAdapter;
+	}
 
 	/**
 	 * Validates the input and, if the values are valid, returns a connection ready to be used.
